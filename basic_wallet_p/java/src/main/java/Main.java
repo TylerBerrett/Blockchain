8eeb7407d5527f9d20b86203a4f873c66a7c03d5
Tyler Berrett <btylerberrett@gmail.com>
2020-03-11 14:29:12 -0600

    add gui to basic_wallet_p

--- basic_wallet_p/java/src/main/java/Main.java
+++ basic_wallet_p/java/src/main/java/Main.java
@@ -2,33 +2,54 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 import javafx.util.Pair;
-import netscape.javascript.JSObject;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-        while (true) {
-            Scanner input = new Scanner(System.in);
-            System.out.println("Enter id");
-            String name = input.nextLine();
-            if (name.equals("q")) {
-                break;
+        JFrame f = new JFrame();
+        JButton b = new JButton("Submit");
+        JTextField tf = new JTextField("Enter Id");
+        JTextArea money = new JTextArea();
+        JTextArea transactions = new JTextArea();
+        JScrollPane scrollPane = new JScrollPane(transactions);
+
+
+        tf.setBounds(100, 100, 200, 40);
+        money.setBounds(100, 160, 200, 16);
+        scrollPane.setBounds(20, 200, 355, 200);
+        b.setBounds(130, 425, 130, 40);
+
+        money.setEditable(false);
+        transactions.setEditable(false);
+
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = tf.getText();
+                Pair<Long, ArrayList<String>> values = getValues(getChain(), name);
+                money.setText("Monies: " + values.getKey());
+                transactions.setText(String.valueOf(formatTransactions(values.getValue())));
             }
-            Pair<Long, ArrayList<String>> values = getValues(getChain(), name);
-            System.out.println(values.getKey());
-            System.out.println(values.getValue());
-        }
+        });
 
+        f.add(tf);
+        f.add(money);
+        f.add(scrollPane);
+        f.add(b);
 
+        f.setSize(400, 500);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 
     private static String getChain() {
@@ -78,5 +99,13 @@ public class Main {
             e.printStackTrace();
         }
         return new Pair<>(total, list);
+    }
+    public static ArrayList<String> formatTransactions(ArrayList<String> list) {
+        ArrayList<String> format = new ArrayList<String>();
+        for (String transaction : list) {
+            String formatted = transaction + "\n";
+            format.add(formatted);
+        }
+        return format;
     }
 }
